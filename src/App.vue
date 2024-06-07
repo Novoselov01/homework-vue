aa7c01863daf3046ac43003abb4a212524e12103
Новоселов Николай Алексеевич <n.a.novoselov@volga.rt.ru>
2024-06-07 11:09:05 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <el-menu
+    ellipsis
+    class="el-menu-popper-demo"
+    mode="horizontal"
+    :popper-offset="16"
+  >
+  <el-menu-item index="0"><router-link to='/'><img
+        style="width: 100px"
+        src="@/assets/img/Your-Logo-here.png"
+        alt="logo"
+      /></router-link></el-menu-item>
+  <div class="flex-grow" />
+  <el-menu-item index="1"><router-link to='/about'>About</router-link></el-menu-item>
+  <el-menu-item index="2"><router-link to='/catalog'>Catalog</router-link></el-menu-item>
+  <el-menu-item index="3"><router-link to='/login'>Sign in</router-link></el-menu-item>
+</el-menu>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+a {
+  text-decoration: none
+}
+.flex-grow {
+  flex-grow: 0.9;
+}
+</style>
